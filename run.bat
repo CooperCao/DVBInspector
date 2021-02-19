cec2f1fb91ceb06371551cc002311ce18c055478
caopeng <caopeng@skyworth.com>
2021-02-19 10:10:45 +0800

    modify log config & run 1.60

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 set JAVA_HOME=C:\Program Files\AdoptOpenJDK\jdk-11.0.8.10-hotspot
-set JVM_FLAGS=-Xmx4g -Xms4g
-java -jar %JVM_FLAGS% .\target\DVBinspector-1.15.0-SNAPSHOT.jar 
+set JVM_FLAGS=-Xmx4g -Xms4g -Djava.util.logging.config.file=./src/main/resources/logging.properties
+java -jar %JVM_FLAGS% .\target\DVBinspector-1.16.0-SNAPSHOT.jar 
